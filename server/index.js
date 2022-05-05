cbb4123bc6610a799731a109820760cd518cceb8
mrrbrilliant <mrrbrilliantwork@gmail.com>
2022-05-05 23:07:33 +0700

    completed credential sharing encryption

--- server/index.js
+++ server/index.js
@@ -21,6 +21,18 @@ app.get("/scan", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "scan.html"));
 });
 
+app.get("/aes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "aes.html"));
+});
+
+app.get("/ende", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "ende.html"));
+});
+
+app.get("/keypair", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "keypair.html"));
+});
+
 const io = new Server(server, {
     cors: {
         origin: "*",
@@ -32,13 +44,12 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     socket.on("/auth/from-login", (data) => {
         const { email, password } = data;
-        if (password === "123") {
-            const token = jwt.sign({ user: email }, "supersecuresecret");
-            socket.emit("/auth/approved", { token });
-        }
+        const token = jwt.sign({ email }, "supersecuresecret");
+        socket.emit("/auth/form-login-approved", { token });
     });
     socket.on("/auth/qr-request", (data) => {
         // join a room in order to be able to recieve message when auth approve
+        console.log(data);
         socket.join(data.id);
     });
     socket.on("/auth/qr-scan", (data) => {
@@ -46,63 +57,7 @@ io.on("connection", (socket) => {
         // he already has indentity from this auth token
         // so we can re-issue and new auth token for others
         // const authorization = socket.request.headers.authorization;
-        try {
-            const authorization = lodash.get(
-                socket,
-                ["request", "headers", "authorization"],
-                null
-            );
-
-            if (!authorization) {
-                let error = new Error("Authorization Error");
-                error["detail"] = {
-                    status: "error",
-                    message: "Authorization header is missing",
-                };
-                throw error;
-            }
-
-            const decoded = jwt.verify(authorization, "supersecuresecret");
-            if (!decoded) {
-                let error = new Error("Authorization Error");
-                error["detail"] = {
-                    status: "error",
-                    message: "Invalid JWT token",
-                };
-                throw error;
-            }
-
-            const user = lodash.get(decoded, ["user"], null);
-            if (!user) {
-                let error = new Error("Authorization Error");
-                error["detail"] = {
-                    status: "error",
-                    message: "JWT missing info",
-                };
-                throw error;
-            }
-
-            const token = jwt.sign({ user }, "supersecuresecret");
-
-            if (!token) {
-                let error = new Error("Authorization Error");
-                error["detail"] = {
-                    status: "error",
-                    message: "Unable to sign new token",
-                };
-                throw error;
-            }
-
-            io.to(data.id).emit("/auth/approved", { token });
-        } catch (error) {
-            if (error) {
-                socket.emit("error", error);
-                return;
-            }
-        }
-    });
-    socket.on("hi", (data, callback) => {
-        callback("ho");
+        io.to(data.id).emit("/auth/qr-login-approved", data);
     });
 });
 
